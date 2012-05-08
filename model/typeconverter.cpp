649c5a7cdc662f50f709a24ac64ef993ddb121b0
Pessiva <giovanni.pessiva@gmail.com>
2012-05-08 18:16:49 +0200

    Implemented string/datetime conversion methods in TypeConverter

--- model/typeconverter.cpp
+++ model/typeconverter.cpp
@@ -1,40 +1,141 @@
 #include "typeconverter.h"
+#include <QDebug>
+
+QString TypeConverter::m_errorMsg = "-no errors-";
 
 TypeConverter::TypeConverter()
 {
 }
 
-QDate TypeConverter::stringToDate(QString date)
+QDateTime *TypeConverter::stringToDate(const QString &date)
 {
-    return null;    //TODO
+    // Schema date format: '-'? yyyy '-' mm '-' dd zzzzzz?
+
+    QDateTime *dateTime;
+
+    if(date[0] == '-'){
+        m_errorMsg = "B.C. years are considered invalid";
+        return 0;
+    }
+
+    if(date.length() < 10){
+        m_errorMsg = "Invalid date length (considering a 4 digits year)";
+        return 0;
+    }
+
+    dateTime=new QDateTime(QDateTime::fromString(date.left(10),"yyyy-MM-dd"));
+
+    if(date.length() == 10){
+        // Unspecified timezone
+        dateTime->setTimeSpec(Qt::LocalTime);
+
+    }else{
+        // Timezone format: (('+' | '-') hh ':' mm) | 'Z'
+        int hours;
+        int minutes;
+
+        if(date[10] == 'Z'){
+            // UTC time
+            dateTime->setTimeSpec(Qt::UTC);
+            hours=0;
+            minutes=0;
+
+        }else{
+            // Offset from the UTC time
+            dateTime->setTimeSpec(Qt::OffsetFromUTC);
+            hours=date.mid(10,3).toInt();
+            minutes=date.mid(14,2).toInt();
+
+            if(date[13] != ':'){
+                m_errorMsg = "Invalid hours/minutes separator: " + date[13].toAscii();
+                return 0;
+            }
+
+            if(hours<-14    ||  hours>14){
+                m_errorMsg = "Invalid hours value: "+ QString::number(hours);
+                return 0;
+            }
+
+            if(minutes<0    ||  minutes>59){
+                m_errorMsg = "Invalid minutes value: "+ QString::number(minutes);
+                return 0;
+            }
+
+            if((hours==14    ||  hours==-14)    &&  minutes!=0){
+                m_errorMsg = "Invalid hours/minutes value: " + QString::number(hours) + "/" + QString::number(minutes);
+                return 0;
+            }
+        }
+
+        //qDebug() <<"ora:" + QString::number(hours) + " minuti: " + QString::number(minutes);
+        //if(dateTime->isValid()==false) qDebug() <<"dateTime is invalid!";
+
+        dateTime->setUtcOffset( (hours * 60 + minutes) * 60);
+
+        //if(dateTime->isValid()==false) qDebug() <<"dateTime is invalid!";
+    }
+
+    return dateTime;
 }
 
-QString TypeConverter::dateToString(QDate date)
+QString *TypeConverter::dateToString(const QDateTime &date)
 {
-    return null;    //TODO
+    QString *d=new QString(date.toString("yyyy-MM-dd"));
+    int utcOffset=date.utcOffset();
+
+    if(date.timeSpec() == Qt::UTC   &&  utcOffset == 0){
+        d->append("Z");
+    }else{
+        if(date.timeSpec() == Qt::OffsetFromUTC){
+            if(utcOffset>0){
+                d->append("+");
+            }else{
+                d->append("-");
+                utcOffset*=-1;
+            }
+
+            utcOffset/=60;
+            int hours=utcOffset/60;
+            int minutes=utcOffset%60;
+
+            if(hours<10  &&  hours>-10)
+                d->append("0");
+            d->append(QString::number(hours) + ":");
+            if(minutes<10)
+                d->append("0");
+            d->append(QString::number(minutes));
+        }
+    }
+
+    return d;
 }
 
-QDate TypeConverter::stringToDuration(QString duration)
+QDateTime *TypeConverter::stringToDuration(const QString &duration)
 {
-    return null;    //TODO
+    return 0;    //TODO
 }
 
-QString TypeConverter::durationToString(QDate duration)
+QString *TypeConverter::durationToString(const QDateTime &duration)
 {
-    return null;    //TODO
+    return 0;    //TODO
 }
 
-QTime TypeConverter::stringToTime(QString time)
+QTime *TypeConverter::stringToTime(const QString &time)
 {
-    return null;    //TODO
+    return 0;    //TODO
 }
 
-QString TypeConverter::timeToString(QTime time)
+QString *TypeConverter::timeToString(const QTime &time)
 {
-    return null;    //TODO
+    return 0;    //TODO
 }
 
-bool TypeConverter::validateUri(QString uri)
+bool TypeConverter::validateUri(const QString &uri)
 {
-    return null;    //TODO
+    return QUrl(uri,QUrl::StrictMode).isValid();
+}
+
+QString *TypeConverter::errorMsg()
+{
+    return &m_errorMsg;
 }
