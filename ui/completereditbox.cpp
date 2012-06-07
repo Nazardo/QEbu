84c2a65f1411c370812c01d97034e010d25818ba
Giovanni Pessiva <giovanni.pessiva@gmail.com>
2012-06-07 21:40:33 +0200

    Updated CompleterEditBox with all the autocompletable fields

--- ui/completereditbox.cpp
+++ ui/completereditbox.cpp
@@ -9,53 +9,18 @@ CompleterEditBox::CompleterEditBox(FieldType fieldType, QWidget *parent):
     QStringList wordList;
 
     switch (fieldType) {
-    case organizationName:
-        wordList <<"Rai - RadioTelevisione Italiana";
-        break;
-
-    case publisherName:
-        wordList <<"Rai - RadioTelevisione Italiana";
-        break;
-
-    case providerName:
-        wordList <<"Rai Teche";
-        break;
-
-    case roleTypeLink:
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:";
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:20.1";
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:10.2";
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:10.3";
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:20.10";
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:25.10";
-        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:20.16";
-        break;
-
-    case roleTypeLabel:
-        wordList <<"Executive Producer";
-        wordList <<"Presenter";
-        wordList <<"Distribution Company";
-        wordList <<"Director";
-        wordList <<"Writer";
-        break;
-
-    case mediumTypeLink:
-        wordList <<"urn:ebu:metadata:cs:StorageMediaCodeCS:2007:IML";
-        break;
-
-    case typeLabelTypeLabel:
-        wordList <<"Betacam IMX";
-        break;
-
-    case locatorTypeDefinition:
-        wordList <<"RAI Internal Archive Number";
-        break;
-
     case subjectCode:
+        //ebuCoreMain.coreMetadata.subject.subjectCode
         wordList <<"urn:ebu:metadata:cs:ContentGenreCS:";
+        wordList <<"urn:ebu:metadata:cs:ContentGenreCS:2007:3.1.1.10.4";
         break;
+
+    case locationCode:
+        //ebuCoreMain.coreMetadata.coverage.spatial.location.code
+        wordList <<"urn:ebu:metadata:cs:ISO3166CountryCodeCS:2007:IT";
 
     case language:
+        //ebuCoreMain.coreMetadata.language
         wordList <<"Italian";
         wordList <<"English";
         wordList <<"French";
@@ -63,7 +28,9 @@ CompleterEditBox::CompleterEditBox(FieldType fieldType, QWidget *parent):
         wordList <<"Spanish";
         break;
 
-    case firstPublicationChannel:
+    case channel:
+        //ebuCoreMain.coreMetadata.publicationHistory.firstPublicationChannel
+        //ebuCoreMain.coreMetadata.publicationHistory.repeatChannel
         wordList <<"Rai Uno";
         wordList <<"Rai Due";
         wordList <<"Rai Tre";
@@ -79,6 +46,195 @@ CompleterEditBox::CompleterEditBox(FieldType fieldType, QWidget *parent):
         wordList <<"Rai Sport 1";
         wordList <<"Rai Sport 2";
         wordList <<"EuroNews";
+        wordList <<"Rete 4";
+        wordList <<"Canale 5";
+        wordList <<"Italia 1";
+        wordList <<"La7";
+        break;
+
+
+
+    case contactDetailsType_name_publisher:
+        //ebuCoreMain.coreMetadata.publisher.contactDetails.name
+        wordList <<"Rai - RadioTelevisione Italiana";
+        wordList <<"Mediaset";
+        wordList <<"Telecom Italia Media";
+        wordList <<"Sky Italia";
+        break;
+
+    case contactDetailsType_name_metadataProvider:
+        //ebuCoreMain.metadataProvider.contactDetails.name
+        wordList <<"Rai Teche";
+        break;
+
+    case organisationDetailsType_organizationName:
+        //ebuCoreMain.coreMetadata.creator.organisationDetails.organisationName
+        //ebuCoreMain.coreMetadata.contributor.organisationDetails.organisationName
+        //ebuCoreMain.metadataProvider.organisationDetails.organisationName
+        wordList <<"Rai - RadioTelevisione Italiana";
+        break;
+
+    case typeGroup_typeLink_role:
+        //ebuCoreMain.coreMetadata.creator.role
+        //ebuCoreMain.coreMetadata.publisher.role
+        //ebuCoreMain.coreMetadata.contributor.organisationDetails.role
+        //ebuCoreMain.coreMetadata.contributor.contactDetails.role
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:";
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:20.1";
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:10.2";
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:10.3";
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:20.10";
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:25.10";
+        wordList <<"urn:ebu:metadata:cs:EBURoleCodeCS:2007:20.16";
+        break;
+
+    case typeGroup_typeLabel_role:
+        //ebuCoreMain.coreMetadata.creator.role
+        //ebuCoreMain.coreMetadata.publisher.role
+        //ebuCoreMain.coreMetadata.contributor.organisationDetails.role
+        //ebuCoreMain.coreMetadata.contributor.contactDetails.role
+        wordList <<"Producer";
+        wordList <<"Executive Producer";
+        wordList <<"Author";
+        wordList <<"Presenter";
+        wordList <<"Director";
+        wordList <<"Writer";
+        wordList <<"Distribution Company";
+        break;
+
+    case typeGroup_typeLink_identifier:
+        //ebuCoreMain.coreMetadata.identifier
+        wordList <<"urn:ebu:metadata:cs:IdentifierTypeCodeCS:";
+        wordList <<"urn:ebu:metadata:cs:IdentifierTypeCodeCS:2007:17";
+        break;
+
+    case typeGroup_typeLabel_identifier:
+        //ebuCoreMain.coreMetadata.identifier
+        wordList <<"Archive Number";
+        break;
+
+    case typeGroup_typeLink_medium:
+        //ebuCoreMain.coreMetadata.format.medium
+        wordList <<"urn:ebu:metadata:cs:StorageMediaCodeCS:2007:IML";
+        break;
+
+    case typeGroup_typeLabel_medium:
+        //ebuCoreMain.coreMetadata.format.medium
+        wordList <<"Betacam IMX";
+        break;
+
+    case typeGroup_typeDefinition_locator:
+        //ebuCoreMain.coreMetadata.locator
+        wordList <<"RAI Internal Archive Number";
+        break;
+
+
+
+    case audioFormatType_audioEncoding:
+        wordList <<"AAC";
+        wordList <<"Mp3";
+        break;
+
+    case audioFormatType_audioTrackConfiguration:
+        wordList <<"stereo";
+        wordList <<"2+1";
+        wordList <<"surround";
+        wordList <<"surround (7+1)";
+
+    case elementType_lang:
+        //ebuCoreMainType
+        wordList <<"en";
+        wordList <<"it";
+        wordList <<"fr";
+        wordList <<"ger";
+        wordList <<"sp";
+        break;
+
+    case formatType_mimeType:
+        wordList <<"audio";
+        wordList <<"video";
+        wordList <<"text";
+        wordList <<"application";
+        break;
+
+    case formatType_containerFormat:
+        wordList <<"mp3";
+        wordList <<"wave";
+        wordList <<"Quicktime";
+        wordList <<"ogg";
+        break;
+
+    case identifierType_typeGroup:
+        wordList <<"Main";
+        wordList <<"Alternative";
+        break;
+
+    case identifierType_formatGroup:
+        wordList <<"SMPTE UMID";
+        wordList <<"ISO ISAN";
+        wordList <<"IETF URI";
+        wordList <<"ISRC";
+        wordList <<"custom";
+        break;
+
+    case imageFormatType_imageEncoding:
+        wordList <<"jpg";
+        wordList <<"png";
+        break;
+
+    case languageType_typeGroup:
+        wordList <<"main original language";
+        wordList <<"main dubbed language";
+        wordList <<"additional original language";
+        wordList <<"additional dubbed language";
+        wordList <<"descriptive video information";
+        wordList <<"supplemental commentary";
+        wordList <<"Director's commentary";
+        wordList <<"audio description";
+        wordList <<"supplementary audio programme";
+        wordList <<"educational notes";
+        wordList <<"voice over";
+        wordList <<"original commentary";
+        wordList <<"dubbed commentary";
+        wordList <<"original narration";
+        wordList <<"dubbed narration";
+        wordList <<"dubbed dialogue";
+        wordList <<"interviewer language";
+        wordList <<"interviewee language";
+        wordList <<"text description for the hard-of-hearing";
+        wordList <<"titles";
+        wordList <<"subtitles";
+        wordList <<"song lyrics";
+        wordList <<"sign language";
+        wordList <<"dubbed sign language";
+        wordList <<"transcript";
+        wordList <<"caption";
+        wordList <<"open caption";
+        wordList <<"closed caption";
+        break;
+
+    case typeType_objectType:
+        wordList <<"programme";
+        wordList <<"item";
+        wordList <<"episode";
+        break;
+
+    case videoFormatType_aspectRatio:
+        wordList <<"1:1";
+        wordList <<"3:2";
+        wordList <<"4:3";
+        wordList <<"5:4";
+        wordList <<"14:9";
+        wordList <<"14:10";
+        wordList <<"16:9";
+        wordList <<"16:10";
+        wordList <<"2.35:1";
+        break;
+
+    case videoFormatType_videoEncoding:
+        wordList <<"H.264";
+        wordList <<"Ogg Theora";
+        wordList <<"VP8";
         break;
     }
 
--- ui/completereditbox.h
+++ ui/completereditbox.h
@@ -9,17 +9,35 @@ class CompleterEditBox : public QLineEdit
 public:
 
     enum FieldType {
-        organizationName, // organisationDetails.organisationName
-        publisherName, // publisher.contactDetails.name
-        providerName, // metadataProvider.contactDetails.name
-        roleTypeLink, // role::typeLink
-        roleTypeLabel,  // role::typeLabel
-        mediumTypeLink, // medium::typeLink
-        typeLabelTypeLabel, // typeLabel::typeLabel
-        locatorTypeDefinition, // locator::typeDefinition
-        subjectCode,
+        subjectCode, // coreMetadata.subject.subjectCode
+        locationCode, // coreMetadata.coverage.spatial.location.code
         language, // coreMetadata.language
-        firstPublicationChannel // coreMetadata.firstPublicationChannel
+        channel, // coreMetadata.publicationHistory.firstPublicationChannel
+
+        contactDetailsType_name_metadataProvider, // metadataProvider.contactDetails.name
+        contactDetailsType_name_publisher, // publisher.contactDetails.name
+        organisationDetailsType_organizationName, // organisationDetails.organisationName
+        typeGroup_typeLink_identifier, // identifier::TypeLink
+        typeGroup_typeLabel_identifier, // identifier::TypeLink
+        typeGroup_typeDefinition_locator, // locator::typeDefinition
+        typeGroup_typeLink_medium, // medium::typeLink
+        typeGroup_typeLabel_medium, // typeLabel::typeLabel
+        typeGroup_typeLink_role, // role::typeLink
+        typeGroup_typeLabel_role,  // role::typeLabel
+
+        audioFormatType_audioEncoding,
+        audioFormatType_audioTrackConfiguration,
+        elementType_lang,
+        formatType_mimeType,
+        formatType_containerFormat,
+        identifierType_typeGroup,
+        identifierType_formatGroup,
+        imageFormatType_imageEncoding,
+        languageType_typeGroup,
+        typeType_objectType,
+        videoFormatType_aspectRatio,
+        videoFormatType_videoEncoding
+
     };
 
     explicit CompleterEditBox(enum FieldType fieldType, QWidget *parent = 0);
