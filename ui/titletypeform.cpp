08bc6e4acf59f6d6f2b308cefec372d17c20d98e
cosmos3900 <cosmos3900@gmail.com>
2012-06-11 14:16:51 +0200

    + TitleTypeForm includes tr()

--- ui/titletypeform.cpp
+++ ui/titletypeform.cpp
@@ -44,7 +44,7 @@ TitleTypeForm::TitleTypeForm(TitleType *title,
     this->setLayout(l);
 
     // Doc filter
-    m_textDocumentation->setText("A Title is the ‘main’ name given to a resource e.g. a media item, a media object, or a sequence as specified by the associated title type. It corresponds for a series to the series title, for a programme to the programme title, for an item to the item title, etc.\nTitles are recorded as they appear.\nThe Title is the name by which a resource is formally known and that everyone should use to refer to or search for that particular resource.\nThe Title may be provided in several languages.\nIf present, the attributionDate attribute indicates when the Title was attributed.");
+    m_textDocumentation->setText(tr("A Title is the ‘main’ name given to a resource e.g. a media item, a media object, or a sequence as specified by the associated title type. It corresponds for a series to the series title, for a programme to the programme title, for an item to the item title, etc.\nTitles are recorded as they appear.\nThe Title is the name by which a resource is formally known and that everyone should use to refer to or search for that particular resource.\nThe Title may be provided in several languages.\nIf present, the attributionDate attribute indicates when the Title was attributed."));
     m_editAttributionDate->installEventFilter(this);
     m_textNote->installEventFilter(this);
     m_editTitle->editValue()->installEventFilter(this);
@@ -117,13 +117,13 @@ bool TitleTypeForm::eventFilter(QObject *obj, QEvent *event)
 {
     if (event->type() == QEvent::FocusIn) {
         if ( obj == (QObject*) m_editAttributionDate)
-            m_textDocumentation->setText("The date at which the title was attributed.");
+            m_textDocumentation->setText(tr("The date at which the title was attributed."));
         else if ( obj == (QObject*) m_textNote )
-            m_textDocumentation->setText("A note element to provide additional contextual information.");
+            m_textDocumentation->setText(tr("A note element to provide additional contextual information."));
         else if  (obj == (QObject*) m_editTitle->editValue())
-            m_textDocumentation->setText("Free-text to provide the main title by which the resource is known. The title can be provided in different languages.\nExample: ‘the fifth element’.");
+            m_textDocumentation->setText(tr("Free-text to provide the main title by which the resource is known. The title can be provided in different languages.\nExample: ‘the fifth element’."));
         else if  (obj == (QObject*) m_editTitle->editLang())
-            m_textDocumentation->setText("The language in which the title is provided.");
+            m_textDocumentation->setText(tr("The language in which the title is provided."));
     }
     return QObject::eventFilter(obj, event);
 }
