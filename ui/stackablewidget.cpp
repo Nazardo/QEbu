f03cbcb356b925e562382bdd9adc11dffb8f53fc
nazardo <mauro@nazardo.it>
2012-06-12 09:20:16 +0200

    Align apply|cancel to top and fix Doc base color
    Also set minimum height for m_textDocumentation to a lower value.

--- ui/stackablewidget.cpp
+++ ui/stackablewidget.cpp
@@ -25,7 +25,7 @@ StackableWidget::StackableWidget(QEbuMainWindow *mainWindow,
         QDialogButtonBox *dialogButtonBox = new QDialogButtonBox(
                     QDialogButtonBox::Apply | QDialogButtonBox::Cancel,
                     Qt::Horizontal);
-        lastRow->addWidget(dialogButtonBox, 0, Qt::AlignLeft);
+        lastRow->addWidget(dialogButtonBox, 0, Qt::AlignLeft | Qt::AlignTop);
         QObject::connect(dialogButtonBox->button(QDialogButtonBox::Apply),
                          SIGNAL(clicked()), this, SLOT(applyClicked()));
         QObject::connect(dialogButtonBox, SIGNAL(rejected()),
@@ -35,10 +35,11 @@ StackableWidget::StackableWidget(QEbuMainWindow *mainWindow,
     m_textDocumentation = new QTextEdit;
     m_textDocumentation->setReadOnly(true);
     QPalette* palette = new QPalette();
-    palette->setColor(QPalette::Base,QColor(240,240,240));
+    palette->setColor(QPalette::Base, palette->color(QPalette::Window));
     m_textDocumentation->setFrameShape( QTextEdit::NoFrame );
     m_textDocumentation->setPalette(*palette);
     m_textDocumentation->setFrameShadow( QTextEdit::Plain );
+    m_textDocumentation->setMinimumHeight(80);
     m_textDocumentation->setMaximumSize(300, 500);
     lastRow->addWidget(m_textDocumentation);
     // ... maybe some day there will be other things to add.
